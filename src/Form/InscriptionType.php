dbe262c87d0bc8ddb4c629b31483d06df21f8fbb
Mathis Zerari <mathiszerari@icloud.com>
2023-09-26 15:53:45 +0200

    home page created

--- src/Form/InscriptionType.php
+++ src/Form/InscriptionType.php
@@ -1,8 +1,10 @@
 <?php
+
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType; // Importation du bon type
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class InscriptionType extends AbstractType
@@ -10,7 +12,11 @@ class InscriptionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('pseudo')
+            ->add('pseudo', TextType::class, [
+                'attr' => ['class' => 'text-3xl font-bold underline'],
+                'label' => 'Mathis', // Le libellé du champ
+                'label_attr' => ['class' => 'required'] // Les attributs du libellé
+            ])
             ->add('email')
             ->add('password')
             ->add('add', SubmitType::class)
