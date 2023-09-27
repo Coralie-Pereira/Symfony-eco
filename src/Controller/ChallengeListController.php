14f1cbb67f5a64bb4c5d3caa7c9341383bdf277c
Smachy <pro.diegocdossantos@gmail.com>
2023-09-27 10:06:34 +0200

    init file

--- src/Controller/ChallengeListController.php
+++ src/Controller/ChallengeListController.php
@@ -13,8 +13,19 @@ class ChallengeListController extends AbstractController
     #[Route("/challenge-list", name:"app_challenge_list")]
     public function main()
     {
-        $challenges = $this -> getDoctrine()->getRepository(Challenge::class)->findAll();
-        return $this->render('challenge-list.html.twig', ['challengeList' => $challenges]);
+        // $challenges = $this -> getDoctrine()->getRepository(Challenge::class)->findAll();
+        // return $this->render('challenge-list.html.twig', ['challengeList' => $challenges]);
+        $lineCount = 0;
+        if (($open = fopen("../Data/ecogestes.csv", "r")) !== false) {
+            while (($data = fgetcsv($open, 1000, ",")) !== false) {
+                if ($lineCount >= 2) {
+                    $challenges[] = $data;
+                }
+                $lineCount++;
+            }
+            return $this->render('challenge-list.html.twig', ['challengeList' => $challenges]);
+            
+        }
     }
 
 }
