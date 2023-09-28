893103203686047a5adbc9dee104739c24f43e92
Smachy <pro.diegocdossantos@gmail.com>
2023-09-28 21:51:39 +0200

    finished or current challenges not in main challenges list

--- src/Controller/ChallengeListController.php
+++ src/Controller/ChallengeListController.php
@@ -10,12 +10,16 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ChallengeListController extends AbstractController
 {
+
+    private $challenges;
+
     #[Route("/challenge-list", name: "app_challenge_list")]
     public function main()
     {
         $userCurrentChallenges = [];
         $userFinishChallenges = [];
-        $challenges = $this->getDoctrine()->getRepository(Challenge::class)->findAll();
+        $this->challenges = $this->getDoctrine()->getRepository(Challenge::class)->findAll();
+        
 
         if ($this->getUser()) {
             $userCurrentChallenges = $this->getCurrentChallenges();
@@ -23,8 +27,8 @@ class ChallengeListController extends AbstractController
         if ($this->getUser()) {
             $userFinishChallenges = $this->getFinishChallenges();
         }
-
-        return $this->render('challenge-list.html.twig', ['challengeList' => $challenges, 'userCurrentChallenges' => $userCurrentChallenges, 'finishChallenges' => $userFinishChallenges]);
+    
+        return $this->render('challenge-list.html.twig', ['challengeList' => $this->challenges, 'userCurrentChallenges' => $userCurrentChallenges, 'finishChallenges' => $userFinishChallenges]);
     }
 
     public function getCurrentChallenges()
@@ -36,6 +40,11 @@ class ChallengeListController extends AbstractController
             if ($userChallengeArray->getStatus() != 1) {
                 unset($userChallengesArray[$key]);
             }
+
+            if(in_array($userChallengeArray->getChallenge(), $this->challenges)){
+                unset($this->challenges[array_search($userChallengeArray->getChallenge(),$this->challenges)]);
+            }
+           
         }
         return $userChallengesArray;
     }
@@ -49,8 +58,13 @@ class ChallengeListController extends AbstractController
             if ($userChallengeArray->getStatus() != 2) {
                 unset($userChallengesArray[$key]);
             }
+            if(in_array($userChallengeArray->getChallenge(), $this->challenges)){
+                unset($this->challenges[array_search($userChallengeArray->getChallenge(),$this->challenges)]);
+            }
         }
         return $userChallengesArray;
     }
+
+  
 
 }
