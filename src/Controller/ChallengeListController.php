fd9ba6a689c379e40377e593c2c2572a35e2df6d
Smachy <pro.diegocdossantos@gmail.com>
2023-09-28 13:58:35 +0200

    current challenges and list challenges

--- src/Controller/ChallengeListController.php
+++ src/Controller/ChallengeListController.php
@@ -16,8 +16,16 @@ class ChallengeListController extends AbstractController
     #[Route("/challenge-list", name:"app_challenge_list")]
     public function main()
     {
+        $userChallenges = $this -> getCurrentChallenges();
         $challenges = $this -> getDoctrine()->getRepository(Challenge::class)->findAll();
-        return $this->render('challenge-list.html.twig', ['challengeList' => $challenges]);
+        return $this->render('challenge-list.html.twig', ['challengeList' => $challenges, 'userChallenges'=>$userChallenges]);
+    }
+
+    public function getCurrentChallenges(){
+        $user = $this -> getUser();
+        $userChallenges = $user -> getUserChallenges();
+        $userChallengesArray = $userChallenges -> toArray();
+        return $userChallengesArray;
     }
 
 }
