ce427187acbad96acda9e646a63485a7005bd673
Smachy <pro.diegocdossantos@gmail.com>
2023-09-28 11:48:31 +0200

    choose challenge is working

--- src/Controller/ChallengeListController.php
+++ src/Controller/ChallengeListController.php
@@ -16,20 +16,8 @@ class ChallengeListController extends AbstractController
     #[Route("/challenge-list", name:"app_challenge_list")]
     public function main()
     {
-        // $challenges = $this -> getDoctrine()->getRepository(Challenge::class)->findAll();
-        $lineCount = 0;
-        if (($open = fopen("../data/ecogestes.csv", "r")) !== false) {
-            while (($data = fgetcsv($open, 1000, ",")) !== false) {
-                if ($lineCount >= 2) {
-                    $challenges[] = $data;
-                }
-                
-                $lineCount++;
-            }
-            return $this->render('challenge-list.html.twig', ['challengeList' => $challenges]);
-            
-        }
-        // return $this->render('challenge-list.html.twig', ['challengeList' => [1,2,3]]);
+        $challenges = $this -> getDoctrine()->getRepository(Challenge::class)->findAll();
+        return $this->render('challenge-list.html.twig', ['challengeList' => $challenges]);
     }
 
 }
---
+++ src/Controller/UserChallengeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+use App\Entity\User;
+use App\Entity\Challenge;
+use App\Entity\UserChallenge;
+use App\Form\UserChallengesType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserChallengeController extends AbstractController
+{
+    #[Route("/add-challenge/{challenge_id}", name:"app_add_challenge")]
+    public function addUserChallenge(Request $request, $challenge_id)
+    {
+        $user = $this-> getUser();
+        $challenge = $this -> getDoctrine()->getRepository(Challenge::class)->find($challenge_id);
+        $user_challenge = new UserChallenge();
+        $user_challenge->setChallenge($challenge);
+        $user_challenge->setUser($user);
+        $user_challenge->setStatus(1);//En cours
+        $user_challenge->setCreatedAt(new \DateTimeImmutable());
+        $user ->addUserChallenge($user_challenge);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user_challenge); 
+        $em->flush();
+
+        return $this->redirectToRoute('app_challenge_list');
+    } 
+}
